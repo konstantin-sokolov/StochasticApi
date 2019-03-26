0d6472a5cbfe4a3c63392c31cf3e8975b9219eca
kvsokolov <k.v.phyxius@gmail.com>
2019-03-26 03:59:11 +0300

    -additional verification

--- StochasticChartApplication/StochasticChartApplication/MainWindowViewModel.cs
+++ StochasticChartApplication/StochasticChartApplication/MainWindowViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Threading;
 using EventApi.Implementation.DataProviders;
 using Generators;
 using Microsoft.Practices.Prism.Commands;
 using Microsoft.Practices.Prism.Mvvm;
+using NLog;
 using StochasticUi.ViewModel;
 using StochasticUi.ViewModel.GeneratorArgs;
 
@@ -15,6 +18,7 @@ namespace StochasticChartApplication
     {
         private readonly GeneratorFactory _generatorFactory;
         private readonly DensityViewModelFactory _viewModelFactory;
+        private readonly ILogger _logger;
         private readonly long _defaultArrayDataSize = 1000 * 1000 * 10;
         private readonly long _defaultMmfDataSize = 100L * 1000L * 1000L;
         private readonly Dispatcher _uiDispatcher;
@@ -27,11 +31,12 @@ namespace StochasticChartApplication
         private bool _isProcessingData;
         private EventDensityViewModel _eventsDensityViewModel;
 
-        public MainWindowViewModel(GeneratorFactory generatorFactory, DensityViewModelFactory viewModelFactory)
+        public MainWindowViewModel(GeneratorFactory generatorFactory, DensityViewModelFactory viewModelFactory,ILogger logger)
         {
             _uiDispatcher = Dispatcher.CurrentDispatcher;
             _generatorFactory = generatorFactory;
             _viewModelFactory = viewModelFactory;
+            _logger = logger;
             GenerateDataCommand = new DelegateCommand(GenerateData);
             SkipSessionCommand = new DelegateCommand(SkipSession);
             _arrayGeneratorArgsViewModel = new ArrayGeneratorArgsViewModel()
@@ -47,22 +52,53 @@ namespace StochasticChartApplication
             ActiveArgsViewModel = _arrayGeneratorArgsViewModel;
         }
 
-        private async Task GenerateDataAsync()
+        private async Task GenerateDataAsync(long size, object[] parameters)
         {
             ProgressValue = 0;
             ProgressStatus = $"Test set generation: {ProgressValue}%";
             IsProcessingData = true;
             var generator = _generatorFactory.GetGenerator(ProviderType);
             generator.EventGenerateProgressChanged += OnEventGenerateProgressChanged;
-            var dataProvider = await generator.GenerateDataProviderAsync(_activeArgsViewModel.GetCollectionSize(), _activeArgsViewModel.GetParameters());
-            generator.EventGenerateProgressChanged -= OnEventGenerateProgressChanged;
-            IsProcessingData = false;
-            EventDensityViewModel = _viewModelFactory.GetViewModel(dataProvider);
+            try
+            {
+                var dataProvider = await generator.GenerateDataProviderAsync(size,parameters);
+                IsProcessingData = false;
+                EventDensityViewModel = _viewModelFactory.GetViewModel(dataProvider);
+            }
+            catch (Exception e)
+            {
+                _logger.Error($"GenerateDataError:{e.Message} - {e.StackTrace}");
+                MessageBox.Show($"Exception while generate data:{e.Message}", "Error", MessageBoxButton.OK);
+            }
+            finally
+            {
+                generator.EventGenerateProgressChanged -= OnEventGenerateProgressChanged;
+            }
+
         }
 
         private void GenerateData()
         {
-            GenerateDataAsync();
+            var size = _activeArgsViewModel.GetCollectionSize();
+            var parameters = _activeArgsViewModel.GetParameters();
+            if (VerifyParams(size, parameters))
+                GenerateDataAsync(size, parameters);
+        }
+
+        private bool VerifyParams(long size, object[] parameters)
+        {
+            string message = null;
+            if (size < 2)
+                message = "Collection can't be empty";
+            if (size % 2 != 0)
+                message = "Collection size should be even number";
+            if (message != null)
+            {
+                MessageBox.Show(message, "Warning", MessageBoxButton.OK);
+                return false;
+            }
+
+            return true;
         }
 
         private void SkipSession()
